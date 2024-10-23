9c70d595ae8f6993d691a761fc3b1a998347ef35
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2024-10-23 10:54:37 +0530

    Update add_two_num.c

--- Basic/add_two_num.c
+++ Basic/add_two_num.c
@@ -1,3 +1,21 @@
+/*
+    Program: Sum of Two Integers
+    Author: Arindam Ghosh
+    
+    This program prompts the user to enter two integers and calculates their sum. 
+    It demonstrates basic input and output operations in C, as well as arithmetic operations.
+    
+    The program follows these steps:
+    1. It declares three integer variables: num1, num2, and sum.
+    2. The user is prompted to input two integers.
+    3. The program calculates the sum of the two integers and stores the result in the variable 'sum'.
+    4. Finally, it outputs the result, displaying both the original integers and their sum.
+    
+    Example:
+    If the user inputs num1 = 7 and num2 = 5,
+    The program will output: "The sum of 7 and 5 is: 12".
+*/
+
 #include <stdio.h>
 
 int main() {
