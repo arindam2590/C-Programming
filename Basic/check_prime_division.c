77eeba551202842d4bc00c18965e775ee0adf9c2
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2024-10-23 10:50:21 +0530

    Update check_prime_division.c

--- Basic/check_prime_division.c
+++ Basic/check_prime_division.c
@@ -1,3 +1,25 @@
+/*
+    Program: Check if a Number is Prime
+    Author: Arindam Ghosh
+    
+    This program checks whether a given positive integer is a prime number using the 
+    simple trial division method. A prime number is a natural number greater than 1 
+    that cannot be formed by multiplying two smaller natural numbers. In other words, 
+    a prime number is only divisible by 1 and itself.
+
+    In this program:
+    - The user is prompted to enter a positive integer.
+    - The program handles special cases (numbers less than or equal to 1).
+    - A simple trial division approach is used to check for factors of the input number 
+      by iterating from 2 up to the square root of the number.
+    - If any number in this range divides the input number evenly, it is determined that 
+      the number is not prime.
+    
+    Example:
+    If the user inputs 29, the program will determine that 29 is a prime number. 
+    If the user inputs 30, it will determine that 30 is not a prime number.
+*/
+
 #include <stdio.h>
 
 int main() {
@@ -16,8 +38,8 @@ int main() {
     // Simple trial division check for factors
     for (i = 2; i * i <= num; i++) {
         if (num % i == 0) {
-            isPrime = 0;
-            break;
+            isPrime = 0;  // Set flag to 0 if a divisor is found
+            break;        // No need to check further
         }
     }
 
