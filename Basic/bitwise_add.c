bb576283ad7f7776b46d90e4b29c0bda4b24a358
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2024-10-23 10:53:37 +0530

    Update bitwise_add.c

--- Basic/bitwise_add.c
+++ Basic/bitwise_add.c
@@ -1,9 +1,35 @@
+/*
+    Program: Bitwise Addition of Two Integers
+    Author: Arindam Ghosh
+    
+    This program demonstrates how to add two integers using bitwise operators 
+    instead of the conventional addition operator (+). The approach utilizes 
+    the properties of bitwise operations to calculate the sum.
+
+    Key Concepts:
+    - The addition is performed without using the '+' operator.
+    - Bitwise AND (&) is used to calculate the carry bits.
+    - Bitwise XOR (^) is used to calculate the sum of the bits without carry.
+    - The carry is then shifted left (<<) to align it with the corresponding 
+      bits for the next addition iteration.
+
+    The program will:
+    - Prompt the user to enter two integers.
+    - Use a custom `add` function that performs addition using bitwise operations.
+    - Display the result of the addition.
+
+    Example:
+    If the user inputs the integers 5 and 10, the program will output:
+    The sum of 5 and 10 is: 15.
+*/
+
 #include <stdio.h>
 
 int add(int a, int b) {
+    int carry;
     while (b != 0) {
         // Carry is calculated by ANDing the bits of a and b
-        int carry = a & b;
+        carry = a & b;
 
         // Sum is calculated using XOR, which adds bits without considering the carry
         a = a ^ b;
@@ -15,7 +41,7 @@ int add(int a, int b) {
 }
 
 int main() {
-    int num1, num2;
+    int num1, num2, sum;
 
     // Input two integers from the user
     printf("Enter the first integer: ");
@@ -25,7 +51,7 @@ int main() {
     scanf("%d", &num2);
 
     // Call the bitwise addition function
-    int sum = add(num1, num2);
+    sum = add(num1, num2);
 
     // Print the sum
     printf("The sum of %d and %d is: %d\n", num1, num2, sum);
