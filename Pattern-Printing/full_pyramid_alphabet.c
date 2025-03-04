bc84cc51f3e880b686f05aeaeeca5c7e33fb323d
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2025-03-04 16:00:26 +0530

    Add files via upload

--- Pattern-Printing/full_pyramid_alphabet.c
+++ Pattern-Printing/full_pyramid_alphabet.c
@@ -1,6 +1,6 @@
 /*
     Program: Print Full Pyramid Pattern Using Alphabets
-    Author: [Your Name]
+    Author: Arindam Ghosh
 
     Description:
     This program prints a full pyramid pattern using alphabets instead of asterisks (`*`).
